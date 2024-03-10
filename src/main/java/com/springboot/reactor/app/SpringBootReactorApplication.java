43cef58f219a0a843f9aea1b5939743e91d8cbac
D.Q.R <davidquijanoramos@gmail.com>
2024-03-09 22:50:41 -0500

    El Operador Filter

--- src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
@@ -21,16 +21,17 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<Usuario> nombres = Flux.just("Andres", "Pedro","Maria", "Diego", "Juan")
-				.map(nombre -> new Usuario(nombre.toUpperCase(), null))
+		Flux<Usuario> nombres = Flux.just("Andres Guzman", "Pedro Fulano","Maria Mengano", "Diego Diego sultano", "Juan Mengano", "Bruce Lee", "Bruce Willy")
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> usuario.getNombre().toLowerCase().equals("bruce"))
 				.doOnNext(usuario -> {
 					if(usuario == null) {
 						throw new RuntimeException("Nombre no puede ser vacio");
 					}
-					System.out.println(usuario.getNombre());					
+					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));					
 				})
 				.map(usuario -> {
-					String nombre = usuario.getNombre().toUpperCase();
+					String nombre = usuario.getNombre().toLowerCase();
 					usuario.setNombre(nombre);
 					return usuario;
 				});
