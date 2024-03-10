617dff8a8bb8103e71264b434988fb843081e619
D.Q.R <davidquijanoramos@gmail.com>
2024-03-09 22:13:13 -0500

    El Operador map 2da parte

--- src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.springboot.reactor.app.model.Usuario;
+
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -18,20 +21,22 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Andres", "Pedro","Maria", "Diego", "Juan")
-//				.doOnNext(System.out::println);
-				.doOnNext(e -> {
-					if(e.isEmpty()) {
+		Flux<Usuario> nombres = Flux.just("Andres", "Pedro","Maria", "Diego", "Juan")
+				.map(nombre -> new Usuario(nombre.toUpperCase(), null))
+				.doOnNext(usuario -> {
+					if(usuario == null) {
 						throw new RuntimeException("Nombre no puede ser vacio");
 					}
-					System.out.println(e);					
+					System.out.println(usuario.getNombre());					
 				})
-				.map(nombre -> {
-					return nombre.toUpperCase();
+				.map(usuario -> {
+					String nombre = usuario.getNombre().toUpperCase();
+					usuario.setNombre(nombre);
+					return usuario;
 				});
 		
 
-		nombres.subscribe(e -> log.info(e),
+		nombres.subscribe(e -> log.info(e.toString()),
 				error -> log.error(error.getMessage()),				
 				new Runnable() {
 					
