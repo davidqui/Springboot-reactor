52c1dd93d9088cf2ade1fcdee49f98d490b4c527
Davidqui <davidquijanoramos@gmail.com>
2024-03-09 20:58:46 -0500

    El Metodo Subcribe
    Se realiza el primer método

---
+++ src/main/java/com/springboot/reactor/app/SpringBootReactorApplication.java
@@ -0,0 +1,38 @@
+package com.springboot.reactor.app;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner{
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Andres", "Pedro","", "Diego", "Juan")
+//				.doOnNext(System.out::println);
+				.doOnNext(e -> {
+					if(e.isEmpty()) {
+						throw new RuntimeException("Nombre no puede ser vacio");
+					}{
+
+					System.out.println(e);
+					}
+				});
+		
+//		nombres.subscribe(log::info);
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()));
+		
+	}
+
+}
